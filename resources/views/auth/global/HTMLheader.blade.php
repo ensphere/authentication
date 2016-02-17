3f8efb5d571fd545e84e8b3a0b68bfc0f16d8cad
Luke Snowden <luke@purposemedia.co.uk>
2016-02-17 14:31:55 +0000

    removed bits

--- resources/views/auth/global/HTMLheader.blade.php
+++ resources/views/auth/global/HTMLheader.blade.php
@@ -6,9 +6,7 @@
 		<meta name="description" content="" />
 		<link href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 		@include('css-loader')
-		<link rel="stylesheet" href="/css/packages.all.css">
 		@include('js-loader')
-		<script src="/js/packages.all.js"></script>
 
 	</head>
 	<body>
